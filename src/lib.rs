31dadedc81896bea611974eeb740cc78df2cae6b
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2024-05-15 14:20:47 -0600

    add introductory explanation examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,176 @@
-//! Utilities to use a struct with a flexible array member.
+/*! [`FamBox`] datastructure to enable ergonomic and safe usage of c structs with a [flexible array member](https://en.wikipedia.org/wiki/Flexible_array_member).
+
+[`FamBox`] comes in several flavors:
+- [`FamBoxOwned`] for owned data.
+- [`FamBoxMut`] for exclusively referenced data.
+- [`FamBoxShared`] for aliased data.
+
+Say you have this c struct:
+```c
+struct protocol_msg {
+    uint8_t version;     /* Protocol version */
+    uint8_t ty;          /* Protocol message type */
+    uint16_t length;     /* Length in bytes including the flexible array member. */
+    uint8_t elements[0]; /* msg data */
+};
+```
+which [bindgen](https://crates.io/crates/bindgen) will translate to
+```rust
+# struct __IncompleteArrayField<T>([T;0]);
+#[repr(C)]
+pub struct protocol_msg {
+    #[doc = " Protocol version"]
+    version: u8,
+    #[doc = " Protocol message type"]
+    ty: u8,
+    #[doc = " Length in bytes including the flexible array member."]
+    length: u16,
+    #[doc = " msg data"]
+    elements: __IncompleteArrayField<u8>,
+}
+```
+A [`FamBoxOwned`] can be used to safely construct a new instance of this type.
+```rust
+# #![allow(non_camel_case_types)]
+use core::mem::size_of;
+use fambox::FamBoxOwned;
+#
+// To be used safely, [`protocol_msg`] can't provide any safe mutable access to `length`.
+pub use encapsulated_struct::protocol_msg;
+mod encapsulated_struct {
+    # use hidden::*;
+    # pub mod hidden {
+    #   #[derive(Debug)]
+    #     pub struct Error;
+    #     #[derive(Debug, PartialEq)]
+    #     pub struct __IncompleteArrayField<T>([T;0]);
+    #     impl<T> __IncompleteArrayField<T> {
+    #         pub fn new() -> Self {
+    #             Self([])
+    #         }
+    #     }
+    # }
+    use core::mem::size_of;
+    use fambox::FamHeader;
+
+    # #[repr(C)]
+    # #[derive(Debug, PartialEq)]
+    # pub struct protocol_msg {
+    #     #[doc = " Protocol version"]
+    #     pub version: u8,
+    #     #[doc = " Protocol message type"]
+    #     pub ty: u8,
+    #     #[doc = " Length in bytes including the flexible array member."]
+    #     length: u16,
+    #     #[doc = " msg data"]
+    #     elements: __IncompleteArrayField<u8>,
+    # }
+    #
+    pub struct InvalidLength;
+
+    # impl From<InvalidLength> for Error {
+    #     fn from(_: InvalidLength) -> Self {
+    #         Error
+    #     }
+    # }
+    #
+    impl protocol_msg {
+        pub fn new(version: u8, ty: u8, buffer_len: usize) -> Result<Self, InvalidLength> {
+            Ok(Self {
+                version,
+                ty,
+                length: (size_of::<Self>()
+                    + buffer_len * size_of::<<Self as FamHeader>::Element>())
+                .try_into()
+                .or(Err(InvalidLength))?,
+                elements: __IncompleteArrayField::new(),
+            })
+        }
+        pub fn length(&self) -> &u16 {
+            &self.length
+        }
+    }
+
+    /// Safety:
+    /// `protocol_msg` doesn't expose a mutable setter which would make the length inconsistent.
+    unsafe impl FamHeader for protocol_msg {
+        type Element = u8;
+
+        fn fam_len(&self) -> usize {
+            let bytes_in_fam = usize::from(self.length) - size_of::<Self>();
+            bytes_in_fam / size_of::<Self::Element>()
+        }
+    }
+}
+# fn test() -> Result<(), encapsulated_struct::hidden::Error> {
+let data_buffer = [0, 1, 2, 3, 4, 5];
+let header = encapsulated_struct::protocol_msg::new(1, 2, data_buffer.len())?;
+let header_and_fam = FamBoxOwned::from_fn(header, |i| data_buffer[i]);
+let header = encapsulated_struct::protocol_msg::new(1, 2, data_buffer.len())?;
+assert_eq!(header_and_fam.header(), &header);
+assert_eq!(header_and_fam.fam(), data_buffer);
+assert_eq!(
+    usize::from(*header_and_fam.header().length()),
+    size_of::<protocol_msg>() + core::mem::size_of_val(&data_buffer)
+);
+# Ok(())
+# }
+# test().unwrap()
+```
+
+or a [`FamBoxShared`]/[`FamBoxMut`] can be used to easily access and manipulate a fam containing struct from c
+```rust,no_run
+# use core::ptr::NonNull;
+# use fambox::{FamBoxShared, FamBoxMut, FamHeader};
+#
+# #[derive(Debug, PartialEq)]
+# struct __IncompleteArrayField<T>([T; 0]);
+# #[repr(C)]
+# #[derive(Debug, PartialEq)]
+# pub struct protocol_msg {
+#     #[doc = " Protocol version"]
+#     version: u8,
+#     #[doc = " Protocol message type"]
+#     ty: u8,
+#     #[doc = " Length in bytes including the flexible array member."]
+#     length: u16,
+#     #[doc = " msg data"]
+#     elements: __IncompleteArrayField<u8>,
+# }
+# unsafe impl FamHeader for protocol_msg {
+#     type Element = u8;
+#
+#     fn fam_len(&self) -> usize {
+#         let bytes_in_fam = usize::from(self.length) - core::mem::size_of::<Self>();
+#         bytes_in_fam / core::mem::size_of::<Self::Element>()
+#     }
+# }
+extern "C" {
+    fn original_header() -> &'static protocol_msg;
+    fn original_data() -> &'static [u8];
+    fn aliased_ptr_from_c() -> NonNull<protocol_msg>;
+    fn alloc_in_c() -> NonNull<protocol_msg>;
+    fn free_in_c(ptr: NonNull<protocol_msg>);
+}
+
+let header_and_fam = unsafe { FamBoxShared::from_raw(aliased_ptr_from_c()) };
+assert_eq!(header_and_fam.as_parts(), unsafe {
+    (original_header(), original_data())
+});
+
+let ptr = unsafe { alloc_in_c() };
+let mut header_and_fam = unsafe { FamBoxMut::from_raw(ptr) };
+assert_eq!(header_and_fam.as_parts(), unsafe {
+    (original_header(), original_data())
+});
+header_and_fam.fam_mut()[2] = 10;
+drop(header_and_fam);
+unsafe { free_in_c(ptr) }
+```
+
+# Feature Flags
+- `serde`: provide `Serialize` and `Deserialize` implementations for `FamBoxOwned`
+*/
 
 // Enable `cargo +nightly doc --all-features` to show all items and their feature requirements.
 // `doccfg` is set by the `build.rs` when supported (nightly)
